279a7ff18efaaf2a3c813f591ec5c634bed82eca
Neil Smith <me@neilsmith.fyi>
2022-03-20 20:22:58 -0500

    chore(setup): setup typescript config, linting and jest testing

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@common/(.*)$": "<rootDir>/common/$1",
+    "^@features/(.*)$": "<rootDir>/features/$1",
+    "^@pages/(.*)$": "<rootDir>/pages/$1",
+    "^@types/(.*)$": "<rootDir>/types/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);
